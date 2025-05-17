8b2f28b2a01d6e51f0ecb04ad25625e4e4bcd1c9
LeeJinHyeong <jfttstudio@gmail.com>
2025-05-18 01:51:20 +0900

    레이턴시 표시

--- demo_viewer.py
+++ demo_viewer.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import cv2
 import numpy as np
 import time
@@ -10,8 +12,9 @@ import dataclass_for_StreamFrameInstance
 from yolox.utils import vis
 from yolox.data.datasets import COCO_CLASSES
 
+
 def visual_from_detection_numpy(stream_frame_instance, cls_conf=0.35):
-    frame =dataclass_for_StreamFrameInstance.load_frame_from_shared_memory(stream_frame_instance, debug=True)
+    frame = dataclass_for_StreamFrameInstance.load_frame_from_shared_memory(stream_frame_instance, debug=True)
     frame = frame.reshape((stream_frame_instance.height, stream_frame_instance.width, 3))
     test_size = (stream_frame_instance.human_detection_tsize, stream_frame_instance.human_detection_tsize)
     ratio = min(test_size[0] / frame.shape[0], test_size[1] / frame.shape[1])
@@ -31,9 +34,10 @@ def visual_from_detection_numpy(stream_frame_instance, cls_conf=0.35):
     vis_res = vis(row_img, bboxes, scores, cls, cls_conf, COCO_CLASSES)  # 프레임에 결과 그려줌
     return vis_res
 
+
 def visual_from_tracking_serial(stream_frame_instance, cls_conf=0.35):
     # 프레임 복원
-    frame =dataclass_for_StreamFrameInstance.load_frame_from_shared_memory(stream_frame_instance, debug=True)
+    frame = dataclass_for_StreamFrameInstance.load_frame_from_shared_memory(stream_frame_instance, debug=True)
     frame = frame.reshape((stream_frame_instance.height, stream_frame_instance.width, 3))
     output = frame.copy()
     serialized_tracks = stream_frame_instance.human_tracking_serial
@@ -56,6 +60,7 @@ def visual_from_tracking_serial(stream_frame_instance, cls_conf=0.35):
 
     return output
 
+
 def demo_viewer(stream_name, frame, debug=False):
     try:
         if frame is None or not isinstance(frame, np.ndarray):
@@ -75,7 +80,7 @@ def _update_imshow_process(stream_queue_for_process, debug=False):
     stream_name = stream_queue_for_process.get().stream_name
     print(f"[INFO] {stream_name} imshow demo process start")
     try:
-        cv2.namedWindow(stream_name, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
+        cv2.namedWindow(stream_name, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_EXPANDED)
         cv2.resizeWindow(stream_name, 800, 600)
 
         while True:
@@ -84,21 +89,26 @@ def _update_imshow_process(stream_queue_for_process, debug=False):
             if instances_per_frame_instance is not None:
                 if instances_per_frame_instance.human_tracking_serial is not None:
                     print(f"[INFO] {stream_name} instances_per_frame is not None")
-                    result_frame = visual_from_tracking_serial(stream_frame_instance=instances_per_frame_instance, cls_conf=0.35)
+                    result_frame = visual_from_tracking_serial(stream_frame_instance=instances_per_frame_instance,
+                                                               cls_conf=0.35)
 
                 elif instances_per_frame_instance.human_detection_numpy is not None:
-                    result_frame = visual_from_detection_numpy(stream_frame_instance=instances_per_frame_instance, cls_conf=0.35)
+                    result_frame = visual_from_detection_numpy(stream_frame_instance=instances_per_frame_instance,
+                                                               cls_conf=0.35)
 
                 else:
-                    result_frame = dataclass_for_StreamFrameInstance.load_frame_from_shared_memory(instances_per_frame_instance, debug=True)
+                    result_frame = dataclass_for_StreamFrameInstance.load_frame_from_shared_memory(
+                        instances_per_frame_instance, debug=True)
 
                 demo_viewer(stream_name, result_frame, debug=debug)
             else:
                 print(f"[INFO] {stream_name} instances_per_frame is None")
                 break
-
-            if cv2.waitKey(2) & 0xFF == ord('q'):
-                break
+            delta = datetime.now() - instances_per_frame_instance.captured_datetime
+            latency_s = int(delta.total_seconds())
+            latency_us = int(delta.total_seconds() * 1_000_000)
+            cv2.displayOverlay(stream_name, f"Latency is {latency_us} µs, about {latency_s} seconds", 2000)
+            cv2.waitKey(1)
         cv2.destroyAllWindows()
         return
     except Exception as e:
@@ -110,24 +120,21 @@ def _update_imshow_process(stream_queue_for_process, debug=False):
         return
 
 
-
-
-
-def _show_imshow_demo(stream_queue,  debug=False):
+def _show_imshow_demo(stream_queue, debug=False):
     stream_viewer_queue_dict = dict()
     stream_viewer_process_set = set()
     try:
-        #sorted_instance=dataclass_for_StreamFrameInstance.sorter(messy_frame_instance_queue=stream_queue, buffer_size=100)
+        # sorted_instance=dataclass_for_StreamFrameInstance.sorter(messy_frame_instance_queue=stream_queue, buffer_size=100)
         print("[INFO] imshow demo start")
         while True:
-            #stream = next(sorted_instance)
+            # stream = next(sorted_instance)
             stream = stream_queue.get()
             stream_name = stream.stream_name
             if stream_name not in stream_viewer_queue_dict:
                 if debug: print(f"[DEBUG] {stream_name} is new in stream_viewer_queue_dict.")
                 stream_viewer_queue_dict[stream_name] = Queue()
                 process = Process(target=_update_imshow_process, args=(stream_viewer_queue_dict[stream_name], debug))
-                #process.daemon = True
+                # process.daemon = True
                 stream_viewer_process_set.add(process)
                 process.start()
                 time.sleep(0.001)
@@ -147,7 +154,7 @@ def _show_imshow_demo(stream_queue,  debug=False):
             print(f"[INFO] {viewer.name} is terminated.")
 
 
-def start_imshow_demo(stream_queue, debug=False,):
+def start_imshow_demo(stream_queue, debug=False, ):
     imshow_demo_thread = Thread(target=_show_imshow_demo, args=(stream_queue, debug))
     imshow_demo_thread.daemon = True
     imshow_demo_thread.start()
